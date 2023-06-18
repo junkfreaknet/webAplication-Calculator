f6855be10af922d3c9624d19c640b9dcc192bee3
junkfreaknet <junk.freak.net@gmail.com>
2023-06-18 22:46:16 +0900

    2023 06 18 22:45

--- scripts/input5_ig/input5_ig.js
+++ scripts/input5_ig/input5_ig.js
@@ -8,17 +8,10 @@
 
     const CS_SMALL_BUY="buy";
     const CS_SMALL_SELL="sell";
-    
-    //var input_X=document.getElementById('id_XAndYinput_X');
-    //var input_Y=document.getElementById('id_XAndY_input_Y');
-    //var btn_calculate=document.getElementById('id_XAndY_button_calculate');
-    //var p_result=document.getElementById('id_XAndY_P_Result');
-    //var p_result_incremental_rate=document.getElementById('id_XAndY_P_Result_Incremental_rate');
-    //btn_calculate.addEventListener('click',calculateXAndY);
 
     var select_buy_or_sell=document.getElementById('id_ig_form_select_buysell');
-    var option_buy=document.getElementById('id_ig_form_option_buy');
-    var option_sell=document.getElementById('id_ig_form_option_sell');
+    //var option_buy=document.getElementById('id_ig_form_option_buy');
+    //var option_sell=document.getElementById('id_ig_form_option_sell');
 
     var input_order_price=document.getElementById('id_XAndYinput_X');
     var input_asking_percentage=document.getElementById('id_XAndY_input_Y');
@@ -32,100 +25,47 @@
     var p_result=document.getElementById('id_XAndY_P_Result');  
 
     var btn_calculate=document.getElementById('id_XAndY_button_calculate');
-    btn_calculate.addEventListener('click',calculateAskingAndStoping());
+    btn_calculate.addEventListener('click',calcAskStop);
 
-    /***
-    var input_order_price=document.getElementById('id_XAndYinput_X');
-    var input_asking_percentage=document.getElementById('id_XAndY_input_Y');
-    var input_stopping_percentage=document.getElementById('id_XAndY_input_z');
 
-    var p_stoping_point=document.getElementById('id_form_ig_p_stopping_point');
-    var p_asking_point=document.getElementById('id_form_ig_p_asking_point');
-    var p_stoping_price=document.getElementById('id_form_ig_p_stopping_price');
-    var p_asking_price=document.getElementById('id_form_ig_p_asking_price');
-***/
+
+    //p_result.textContent="test drive.";
 //
 //
 //
-/***
-    function calculateXAndY(){
+//
+//
+    function calcAskStop(){
 
-        var value_X=input_X.value;
-        var value_Y=input_Y.value;
+        if(select_buy_or_sell.value==CS_SMALL_BUY){
 
-        if (isValidInput(value_X,value_Y)){
-            p_result.textContent="Your asking price is  " + String(GetAskingPrice(Number(value_X),Number(value_Y)));
-            //p_result.textContent="Your asking price is  " + String(GetEPSByRate(value_X,value_Y)) + " %";
-            //p_result_incremental_rate.textContent="Incremental rate is " + String(calculateIncrementalRateBetweenXAndY(value_X,value_Y))+" %";  
         }else
-        {
-            p_result.textContent="input numeric value.";
-        }
+        if(select_buy_or_sell.value==CS_SMALL_SELL){
 
-
-    }
-***/
-
-    function calculateAskingAndStoping(){
-
-
-        var value_select_or_sell=select_buy_or_sell.value;
-        
-        var value_order_price=input_order_price.value;
-        var value_asking_percentage=input_asking_percentage.value;
-        var value_stopping_percentage=input_stopping_percentage.value;
-
-        //start check long or short
-        //buy_or_sell="";
-        if(value_select_or_sell==CS_SMALL_BUY){
-            buy_or_sell=CS_SMALL_BUY;
-            p_result.textContent("buy");
-        }
-        else if(value_select_or_sell==CS_SMALL_SELL){
-            buy_or_sell=CS_SMALL_SELL;
-            p_result.textContent("sell");
-        }else
-        {
-            p_result.textContent="select Buy or Sell"
+        }else{
+            p_result.textContent="select buy or sell.";
             p_result.style.color="red";
             return;
         }
-    
 
-        //buy_or_sell=CS_SMALL_BUY;
+        if(isValidInput(input_order_price.value,input_asking_percentage.value,input_stopping_percentage.value)){
 
-        //end check long or short
+            p_asking_price.textContent="Asking price is:"+String(getAskingPrice(Number(input_order_price.value),Number(input_asking_percentage.value),select_buy_or_sell.value));
+            p_stopping_price.textContent="Stopping price is:"+String(getStoppingPrice(Number(input_order_price.value),Number(input_stopping_percentage.value),select_buy_or_sell.value));            
 
-        if (isValidInput(value_order_price,value_asking_percentage,value_stopping_percentage)){
-            /***p_result.textContent="Your asking price is  " + String(GetAskingPrice(Number(value_X),Number(value_Y)));***/
-            //p_result.textContent="Your asking price is  " + String(GetEPSByRate(value_X,value_Y)) + " %";
-            //p_result_incremental_rate.textContent="Incremental rate is " + String(calculateIncrementalRateBetweenXAndY(value_X,value_Y))+" %";
+            p_asking_point.textContent=String(getAsPoint(Number(input_order_price.value),Number(input_asking_percentage.value)));
+            p_stopping_point.textContent=String(getAsPoint(Number(input_order_price.value),Number(input_stopping_percentage.value)));
 
-            //p_asking_price.textContent=string(getAskingPrice(Number(value_order_price),Number(value_asking_percentage)));
-            //p_stopping_price.textContent=string(getStoppingPrice(Number(value_order_price),Number(value_stoppinging_percentage)));
+            p_asking_point.style.color="green";
+            p_stopping_point.style.color="green";
 
-            p_asking_price.textContent="Asking Price is "+":"+String(getAskingPrice(Number(value_order_price),Number(value_asking_percentage),buy_or_sell))+"";
-            p_stopping_price.textContent="Stoppinging Price is "+":"+String(getStoppingPrice(Number(value_order_price),Number(value_stopping_percentage),buy_or_sell))+"";
-
-
-            p_result.textContent="***ok***";
             p_result.style.color="green";
-
-            if(value_select_or_sell==CS_SMALL_BUY)
-            {
-                p_result.textContent="buy";
-            }else
-            {
-                p_result.textContent="sell";
-            }
-
-        }else
-        {
+            p_result.textContent="***"+select_buy_or_sell.value+"***...OK";
+        }else{
             p_result.textContent="***input numeric value.***";
             p_result.style.color="red";
         }
 
-        //
 
     }
 
@@ -180,6 +120,15 @@ function getStoppingPrice(order_price,rate,buy_or_sell){
     }else{
         rv=order_price+((order_price/100)*rate);
     }
+    return rv;
+
+}
+
+function getAsPoint(order_price,rate){
+
+    let rv=0;
+
+    rv=(order_price/100)*rate;    
     return rv;
 
 }
